9f5ee9799a9eef6f2bf70ca74c7504f9d3981954
ESPE-salema <salema@espe.edu.ec>
2022-06-17 17:23:40 -0500

    Login

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$conexion = mysqli_connect("ec2-44-196-174-238.compute-1.amazonaws.com:5432",
+ "keejhhvspitvnn",
+  "d32d62fbfb7a44d46ec4ef81edec9852436449f004e8de3b086688d6202735c8",
+   "d9c6btka0noaof") or die("Error de conexion..");
+?>
